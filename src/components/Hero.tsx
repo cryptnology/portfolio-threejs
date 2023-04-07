7211b40667b20c6eeefc54234fb44e3256b28eec
cryptnology <jamie@cryptnology.dev>
2023-04-07 12:45:02 +0100

    Create Navbar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div></div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
