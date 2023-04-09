2192fd0cf93a06f87f2fd917cf96ffc05165f520
cryptnology <jamie@cryptnology.dev>
2023-04-09 09:42:32 +0100

    Create Hero section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -45,7 +45,7 @@ const Navbar = () => {
           <img src={logo} alt="logo" className="w-7 h-7 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Jamie&nbsp;
-            <span className="sm:block hidden"> @ Cryptnology</span>
+            <span className="sm:block hidden"> @Cryptnology</span>
           </p>
         </Link>
 
